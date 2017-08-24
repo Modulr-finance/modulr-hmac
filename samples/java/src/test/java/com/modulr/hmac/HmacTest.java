bd27dc4238278bccf64a81ea34b75d161d3ee702
Gabriel Rusu <gabriel.rusu@modulrfinance.com>
2017-08-24 10:47:38 +0100

    Added additional checks

--- samples/java/src/test/java/com/modulr/hmac/HmacTest.java
+++ samples/java/src/test/java/com/modulr/hmac/HmacTest.java
@@ -10,6 +10,7 @@ import java.security.SignatureException;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Date;
+import java.util.Map;
 
 @RunWith(JUnit4.class)
 public class HmacTest {
@@ -28,5 +29,10 @@ public class HmacTest {
 
         modulrAuth.setDate(date);
         Assert.assertEquals("G9zfk3yPn861TKddM6wIxu4u0YU%3D", modulrAuth.generateHmac());
+
+        Map<String, String> headers = modulrAuth.getAuthHeaders();
+        Assert.assertEquals("Signature keyId=\"KNOWN-TOKEN\",algorithm=\"hmac-sha1\",headers=\"date x-mod-nonce\",signature=\"G9zfk3yPn861TKddM6wIxu4u0YU%3D\"", headers.get("Authorization"));
+        Assert.assertEquals("NONCE", headers.get("x-mod-nonce"));
+        Assert.assertEquals("Fri, 04 Aug 2017 10:10:20 GMT", headers.get("Date"));
     }
 }
