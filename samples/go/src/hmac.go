133fb46a24bfb08c2c31c94006a257a26cd82ebb
Stefan Huth <stefan.huth@modulrfinance.com>
2020-11-30 15:34:47 +0000

    got it working

--- samples/go/src/hmac.go
+++ samples/go/src/hmac.go
@@ -1,27 +1,67 @@
 package main
 
-type SignatureError struct {
-	Message string
-}
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"fmt"
+	"github.com/google/uuid"
+	"net/url"
+	"time"
+)
 
-func generate(apiKey string, apiSecret string) (string, *SignatureError) {
+const (
+	ErrorResponse   = ""
+	Algorithm       = "algorithm=\"hmac-sha1\","
+	Headers         = "headers=\"date x-mod-nonce\","
+	SignaturePrefix = "signature=\""
+	SignatureSuffix = "\""
+	DateFormat      = "Mon, 02 Jan 2006 15:04:05 GMT"
+	Date 			= "date: "
+	Nonce = "\nx-mod-nonce: "
+)
+
+var dateNow = time.Now
+
+func generate(apiKey string, apiSecret string, nonce string) (string, *SignatureError) {
 	validationError := validateInput(apiKey, apiSecret)
 	if validationError != nil {
-		return "", validationError
+		return ErrorResponse, validationError
 	}
 
-	return "key", nil
+	return buildSignature(apiKey, apiSecret, nonce)
 }
 
+func buildSignature(apiKey string, apiSecret string, nonce string) (string, *SignatureError) {
+	return buildKeyId(apiKey) + Algorithm + Headers + generateEncodedSignature(apiSecret, nonce), nil
+}
 
-func validateInput (apiKey string, apiSecret string) *SignatureError {
-	if apiKey == "" {
-		return &SignatureError{ "api_key cannot be empty" }
+func buildKeyId(apiKey string) string {
+	return "Signature keyId=\"" + apiKey + "\","
+}
+
+func generateEncodedSignature(apiSecret string, nonce string) string {
+
+	mac := hmac.New(sha1.New, []byte(apiSecret))
+
+	plainSig := retrieveDate() + buildNonce(nonce)
+	fmt.Print("plainSig[" + plainSig + "]")
+	mac.Write([]byte(plainSig))
+	encodedMac := mac.Sum(nil)
+	base64Encoded := base64.URLEncoding.EncodeToString(encodedMac)
+
+	return SignaturePrefix + url.QueryEscape(base64Encoded) + SignatureSuffix
+}
+
+func retrieveDate() string {
+	now := dateNow()
+	return Date + now.Format(DateFormat)
+}
+
+func buildNonce(nonce string) string {
+	if nonce == "" {
+		nonce = uuid.New().String()
 	}
-
-	if apiSecret == "" {
-		return &SignatureError{ "api_secret cannot be empty" }
-	}
-
-	return nil
+	fmt.Print("nonce [" + nonce + "]")
+	return Nonce + nonce
 }
--- samples/go/src/hmac_test.go
+++ samples/go/src/hmac_test.go
@@ -1,21 +1,77 @@
 package main
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestGenerateReturnsAnHMACString(t *testing.T) {
-	var signature, _ = generate("api_key", "api_secret")
-	assert.Equal(t, signature, "key", "generate should return the hmac signature")
+	signature, _ := generate("57502612d1bb2c0001000025276a506e125f464f89132e7ca1773987", "YTUzODc3N2FiODEyNDdiMWE3ZGUwMDU1NDZmNmNmMzA=", "")
+	fmt.Print("\n" + signature)
+	expectedSignature := "Signature keyId=\"api_key\",algorithm=\"hmac-sha1\",headers=\"date x-mod-nonce\",signature=\""
+	assert.Equal(t, expectedSignature, signature[0:86], "generate should return the hmac signature")
 }
 
 func TestGenerateThrowsErrorIfApiKeyIsNull(t *testing.T) {
-	 var _, err = generate("", "api_secret")
+	 _, err := generate("", "api_secret", "")
 	 assert.Equal(t, "api_key cannot be empty", err.Message)
 }
 
 func TestGenerateThrowsErrorIfApiSecretIsNull(t *testing.T) {
-	var _, err = generate("api_key", "")
+	_, err := generate("api_key", "", "")
 	assert.Equal(t, "api_secret cannot be empty", err.Message)
+}
+
+func TestGenerateReturnsSignatureWithKeyId(t *testing.T) {
+	signature, _ := generate("api_key", "api_secret", "")
+	expectedPrefix := "Signature keyId=\"api_key\","
+	hasKeyId := strings.HasPrefix(signature, expectedPrefix)
+	assert.True(t, hasKeyId, "HMAC signature must contain the keyId")
+}
+
+func TestGenerateReturnsSignatureWithAlgorithm(t *testing.T) {
+	signature, _ := generate("api_key", "api_secret", "")
+	expectedAlgorithm := "algorithm=\"hmac-sha1\","
+	actualValue := signature[26:48]
+	assert.Equal(t, expectedAlgorithm, actualValue, "HMAC signature must contain the algorithm used")
+}
+
+func TestGenerateReturnsSignatureWithHeaders(t *testing.T) {
+	signature, _ := generate("api_key", "api_secret", "")
+	expectedHeaders := "headers=\"date x-mod-nonce\","
+	actualValue := signature[48:75]
+	assert.Equal(t, expectedHeaders, actualValue, "HMAC signature must contain the headers")
+}
+
+func TestGenerateReturnsSignatureWithSignatureValue(t *testing.T) {
+	signature, _ := generate("api_key", "api_secret", "")
+	expectedSignature := "signature=\""
+	actualValue := signature[75:86]
+	assert.Equal(t, expectedSignature, actualValue, "HMAC signature must contain the signature")
+}
+
+func TestGenerateReturnsHashedSignature(t *testing.T) {
+	signature, _ := generate("api_key", "api_secret", "")
+	fmt.Print(signature)
+	actualValue := signature[86:117]
+	assert.True(t, actualValue != "", "Encoded HMAC signature should be present")
+}
+
+func TestGenerateAcceptsANonce(t *testing.T) {
+	injectMockDate()
+	signature, _ := generate("api_key", "api_secret", "nonce")
+	actualValue := signature[86:116]
+
+	expected := "GDiVOYxYOn6n7MVcAzREF_Nv-Kw%3D"
+	assert.Equal(t, expected, actualValue, "HMAC signature must contain the signature")
+}
+
+func injectMockDate() {
+	dateNow = func() time.Time {
+		now, _ := time.Parse("2020-01-02", "2020-02-02")
+		return now
+	}
 }
