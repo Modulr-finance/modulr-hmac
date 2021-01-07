85db773e31af0c7d5d479527d11d680cdb0ddefd
Stefan Huth <stefan.huth@modulrfinance.com>
2021-01-07 16:45:38 +0000

    fixed signature and pulled out header construction

--- samples/go/src/hmac_test.go
+++ samples/go/src/hmac_test.go
@@ -3,21 +3,46 @@ package main
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestGenerateReturnsAnHMACString(t *testing.T) {
-	headers, _, _ := generate("api_key", "api_secret", "")
-	expectedSignature := "Signature keyId=\"api_key\",algorithm=\"hmac-sha1\",headers=\"date x-mod-nonce\",headers=\""
+	headers, _ := generate("api_key", "api_secret", "")
+	expectedSignature := "Signature keyId=\"api_key\",algorithm=\"hmac-sha1\",headers=\"date x-mod-nonce\",signature=\""
 	assert.Equal(t, expectedSignature, headers["Authorization"][0:86], "generate should return the hmac headers")
 }
 
+func TestGenerateReturnsADateHeader(t *testing.T) {
+	injectMockDate()
+	headers, _ := generate("api_key", "api_secret", "")
+	expectedDate := "2020-01-02"
+	assert.Equal(t, expectedDate, headers["Date"])
+}
+
+func TestGenerateReturnsANonceHeaderWithExpectedValue(t *testing.T) {
+	nonce := "thisIsTheNonce"
+	headers, _ := generate("api_key", "api_secret", nonce)
+	assert.Equal(t, nonce, headers["x-mod-nonce"])
+}
+
+func TestGenerateReturnsAGeneratedNonceHeaderIfNonceIsEmpty(t *testing.T) {
+	headers, _ := generate("api_key", "api_secret", "")
+	assert.True(t, headers["x-mod-nonce"] != "", "x-mod-nonce header should have been populated")
+}
+
 func TestGenerateThrowsErrorIfApiKeyIsNull(t *testing.T) {
-	 _, err, _ := generate("", "api_secret", "")
+	 _, err := generate("", "api_secret", "")
 	 assert.Equal(t, "api_key cannot be empty", err.Message)
 }
 
 func TestGenerateThrowsErrorIfApiSecretIsNull(t *testing.T) {
-	_, err, _ := generate("api_key", "", "")
+	_, err := generate("api_key", "", "")
 	assert.Equal(t, "api_secret cannot be empty", err.Message)
 }
 
+func injectMockDate() {
+	dateNow = func() time.Time {
+		now, _ := time.Parse(time.RFC1123, "Mon, 02 Jan 2020 15:04:05 GMT")
+		return now
+	}
+}
