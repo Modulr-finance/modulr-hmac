99939bb6d8d487e09dbb055b00e6585ee99f74e1
Stefan Huth <stefan.huth@modulrfinance.com>
2021-01-07 16:45:38 +0000

    fixed signature and pulled out header construction

--- samples/go/src/hmac.go
+++ samples/go/src/hmac.go
@@ -1,21 +1,44 @@
 package main
 
-import "github.com/Modulr-finance/modulr-hmac/src/signature"
+import (
+	"github.com/Modulr-finance/modulr-hmac/src/signature"
+	"github.com/google/uuid"
+	"time"
+)
 
-func generate(apiKey string, apiSecret string, nonce string) (map[string] string, *ValidationError, *signature.SignatureError) {
+const (
+	AuthorizationHeader = "Authorization"
+	DateHeader          = "Date"
+	DateFormat = "2006-01-02"
+	EmptyString     = ""
+	NonceHeader = "x-mod-nonce"
+)
+
+var dateNow = time.Now
+
+func generate(apiKey string, apiSecret string, nonce string) (map[string] string, *ValidationError) {
 	validationError := validateInput(apiKey, apiSecret)
-	headers := make(map[string] string)
+
 	if validationError != nil {
-		return nil, validationError, nil
+		return nil, validationError
 	}
-	signature, err := signature.Build(apiKey, apiSecret, nonce)
-
-	if err != nil {
-		return nil, nil, err
-	}
-
-	headers["Authorization"] = signature
-
-	return headers, nil, nil
+	return buildHeaders(apiKey, apiSecret, nonce), nil
 }
 
+func buildHeaders(apiKey string, apiSecret string, nonce string) map[string] string {
+	headers := make(map[string] string)
+	date := dateNow()
+	nonce = generateNonceIfEmpty(nonce)
+
+	headers[DateHeader] = date.Format(DateFormat)
+	headers[AuthorizationHeader] = signature.Build(apiKey, apiSecret, nonce, date)
+	headers[NonceHeader] = nonce
+	return headers
+}
+
+func generateNonceIfEmpty(nonce string) string {
+	if nonce == EmptyString {
+		nonce = uuid.New().String()
+	}
+	return  nonce
+}
--- samples/go/src/signature/signature_test.go
+++ samples/go/src/signature/signature_test.go
@@ -8,51 +8,49 @@ import (
 )
 
 func TestGenerateReturnsSignatureWithKeyId(t *testing.T) {
-	signature, _ := Build("api_key", "api_secret", "")
+	signature := Build("api_key", "api_secret", "", date())
 	expectedPrefix := "Signature keyId=\"api_key\","
 	hasKeyId := strings.HasPrefix(signature, expectedPrefix)
 	assert.True(t, hasKeyId, "HMAC signature must contain the keyId")
 }
 
 func TestGenerateReturnsSignatureWithAlgorithm(t *testing.T) {
-	signature, _ := Build("api_key", "api_secret", "")
+	signature := Build("api_key", "api_secret", "", date())
 	expectedAlgorithm := "algorithm=\"hmac-sha1\","
 	actualValue := signature[26:48]
 	assert.Equal(t, expectedAlgorithm, actualValue, "HMAC signature must contain the algorithm used")
 }
 
 func TestGenerateReturnsSignatureWithHeaders(t *testing.T) {
-	signature, _ := Build("api_key", "api_secret", "")
+	signature := Build("api_key", "api_secret", "", date())
 	expectedHeaders := "headers=\"date x-mod-nonce\","
 	actualValue := signature[48:75]
 	assert.Equal(t, expectedHeaders, actualValue, "HMAC signature must contain the headers")
 }
 
 func TestGenerateReturnsSignatureWithSignatureValue(t *testing.T) {
-	signature, _ := Build("api_key", "api_secret", "")
+	signature := Build("api_key", "api_secret", "", date())
 	expectedSignature := "signature=\""
 	actualValue := signature[75:86]
 	assert.Equal(t, expectedSignature, actualValue, "HMAC signature must contain the signature")
 }
 
 func TestGenerateReturnsHashedSignature(t *testing.T) {
-	signature, _ := Build("api_key", "api_secret", "")
+	signature := Build("api_key", "api_secret", "", date())
 	actualValue := signature[86:117]
 	assert.True(t, actualValue != "", "Encoded HMAC signature should be present")
 }
 
 func TestGenerateAcceptsANonce(t *testing.T) {
-	injectMockDate()
-	signature, _ := Build("api_key", "api_secret", "nonce")
+	signature := Build("api_key", "api_secret", "nonce", date())
 	actualValue := signature[86:116]
-
-	expected := "dUIUO_JZUOSjsEhEBb_QoedvHic%3D"
+	expected := "YPK-16yaBxuZuciYLGoxS30QBgI%3D"
 	assert.Equal(t, expected, actualValue, "HMAC signature must contain the signature")
 }
 
-func injectMockDate() {
-	dateNow = func() time.Time {
-		now, _ := time.Parse("2020-01-02", "2020-02-02")
-		return now
-	}
+
+func date() time.Time {
+	now, _ := time.Parse(time.RFC1123, "Mon, 02 Jan 2020 15:04:05 GMT")
+	return now
 }
+
