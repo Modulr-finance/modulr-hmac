81290d37794b337d09a3f9f5fe9ab2d4eca0d125
Ritesh Tendulkar <ritesh.tendulkar@modulrfinance.com>
2023-06-08 08:56:37 +0100

    Issue-32: Upgrade libraries
    Also remove https://github.com/request/request due to https://github.com/request/request/issues/3142
    
    Also remove hard-coded API keys in the nodejs sample

--- samples/nodejs/index.js
+++ samples/nodejs/index.js
@@ -1,25 +1,29 @@
 'use strict';
-const request = require('request');
 const signature = require(`./signature`);
 
-const API_KEY = "57502612d1bb2c00010000256c9568f1f7d540e085052b425d46d233";
-const API_SECRET = "Y2Q0MzhmZDE2OWIzNDVkNGE5ZTczOTU3ZTAxZWY5NTc=";
+const API_KEY = "<YOUR-API-KEY>";
+const API_SECRET = "<YOUR-SECRET>";
 
 const signatureHelper = new signature(API_KEY,API_SECRET);
 const signatureResult = signatureHelper.calculate();
-request.get(
-    {
-        url: 'https://api-sandbox.modulrfinance.com/api-sandbox/customers',
-        json: true,
-        headers: signatureResult.getHTTPHeaders()
-    },
-    (err, res, data) => {
-        if (err) {
-            console.error('Error calling API', err);
-        } else if (res.statusCode !== 200) {
-            console.error('Unsuccessful API call, code: ', res.statusCode, ', messsage: ', res.statusMessage, ', body: ', data);
-        } else {
-            console.log('OK response from API, body: ', data);
+
+async function getCustomers(){
+    try {
+        const response = await fetch('https://api-sandbox.modulrfinance.com/api-sandbox/customers',
+            {
+                headers: signatureResult.getHTTPHeaders()
+            })
+
+        const jsonBody = await response.json();
+
+        if (response.ok) {
+            console.log('OK response from API, body: ', jsonBody);
+        }else {
+            console.error('Unsuccessful API call, code: ', response.status,  ', body: ', jsonBody);
         }
-    });
-    
+    }catch(error){
+        console.error('Error calling API', error);
+    }
+}
+
+getCustomers();
